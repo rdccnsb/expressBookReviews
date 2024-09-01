5dc393ff287ec0a7cd2bb6013cf663b7f5d1eaec
Roger <roger_carb@hotmail.com>
2024-09-01 10:06:59 -0400

    Register and Login

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,13 +7,13 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "Yet to be implemented /register"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "Yet to be implemented /"});
 });
 
 // Get book details based on ISBN
